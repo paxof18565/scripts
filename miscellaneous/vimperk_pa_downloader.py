bfbfa283f84d8acd44740b7f8893dba5fac12e49
René Kliment <rene@renekliment.cz>
2015-08-28 17:41:49 +0200

    [vimperk_pa_downloader] updated to the 'new file listing schema' - will be updated to automatically download from all dirs if they keep using it; uses only urllib2 now

--- miscellaneous/vimperk_pa_downloader.py
+++ miscellaneous/vimperk_pa_downloader.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import urllib
 import urllib2
 import re
-import os.path
+import os
 
-downloadDirectory = '/tmp/'
-url = 'http://www.mesto.vimperk.cz/audio/rozhlas/'
+paURL = "http://www.mesto.vimperk.cz/audio/rozhlas/"
+downloadDirectory = '/tmp/vimperk_pa/'
 
-req = urllib2.Request(url)
-response = urllib2.urlopen(req)
-page = response.read()
+def downloadFromURL(url, directory):
+	if (not os.path.exists(directory)):
+		os.makedirs(directory)
 
-regex = re.compile(r'"((\w|%)+\.wav)"', re.UNICODE)
-matches = regex.findall(page)
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+	page = response.read()
 
-for match in matches:
-	file = match[0]
+	regex = re.compile(r'"((\w|%)+\.wav)"', re.UNICODE)
+	matches = regex.findall(page)
 
-	if (not os.path.isfile(downloadDirectory + '/' + file)):
-		testfile = urllib.URLopener()
-		testfile.retrieve(url + file, downloadDirectory + '/' + file)
+	for match in matches:
+		file = match[0]
+
+		if (not os.path.isfile(directory + '/' + file)):
+			remote_file = urllib2.urlopen(url + file)
+			local_file = open(directory + '/' + file, "wb")
+			local_file.write(remote_file.read())
+
+downloadFromURL(paURL, downloadDirectory)
+downloadFromURL(paURL + "2015/", downloadDirectory + "2015/")
