6a7f53213fa31879fd3cadeb2aea67a71f3459ec
René Kliment <rene@renekliment.cz>
2017-02-18 14:38:34 +0100

    [vimperk_pa_downloader] python 3 support, logging, switch to requests, download all years (up to the current) and other code improvements

--- miscellaneous/vimperk_pa_downloader.py
+++ miscellaneous/vimperk_pa_downloader.py
@@ -1,31 +1,58 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib2
-import re
 import os
+import sys
+import datetime
+import logging
+import re
 
-paURL = "http://www.mesto.vimperk.cz/audio/rozhlas/"
-downloadDirectory = '/tmp/vimperk_pa/'
+import requests
 
-def downloadFromURL(url, directory):
-	if (not os.path.exists(directory)):
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def vimperk_pa_download(year, directory):
+
+	PA_URL = 'http://www.mesto.vimperk.cz/audio/rozhlas/'
+
+	url = PA_URL + str(year) + '/'
+	if year == 2014:
+		url = PA_URL
+
+	logger.info('Downloading from %s into %s', url, directory)
+
+	if not os.path.exists(directory):
+		logger.info('Creating directory %s', directory)
 		os.makedirs(directory)
 
-	req = urllib2.Request(url)
-	response = urllib2.urlopen(req)
-	page = response.read()
+	response = requests.get(url)
 
 	regex = re.compile(r'"((\w|%)+\.wav)"', re.UNICODE)
-	matches = regex.findall(page)
+	matches = regex.findall(response.content.decode(encoding=response.encoding))
+
+	logger.info('Found %s files', len(matches))
 
 	for match in matches:
-		file = match[0]
+		file_item = match[0]
+		local_filename = os.path.join(directory, file_item)
 
-		if (not os.path.isfile(directory + '/' + file)):
-			remote_file = urllib2.urlopen(url + file)
-			local_file = open(directory + '/' + file, "wb")
-			local_file.write(remote_file.read())
+		logger.debug('Processing file %s', file_item)
 
-downloadFromURL(paURL, downloadDirectory)
-downloadFromURL(paURL + "2015/", downloadDirectory + "2015/")
+		if not os.path.isfile(local_filename):
+			logger.info('Downloading %s', file_item)
+
+			remote_file = requests.get(url + file_item)
+			local_file = open(local_filename, 'wb')
+			local_file.write(remote_file.content)
+		else:
+			logger.debug('File %s already on local storage', file_item)
+
+
+if __name__ == '__main__':
+
+	download_directory = os.path.join(sys.argv[1], '') if len(sys.argv) == 2 else '/tmp/vimperk_pa/'
+
+	for year in list(range(2014, datetime.date.today().year + 1)):
+		vimperk_pa_download(year, download_directory + str(year) + '/')
